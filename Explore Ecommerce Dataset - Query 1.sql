3ef91856f9ed8ccf22a2d0646e4ce12d4b42b7b6
Ho Nguyen Thien Phu <100189231+honguyenthienphu@users.noreply.github.com>
2024-11-24 12:52:22 +0700

    Delete Explore Ecommerce Dataset - Query 1.sql

--- Explore Ecommerce Dataset - Query 1.sql
+++
@@ -1,9 +0,0 @@
-select
-  format_date("%Y%m", parse_date("%Y%m%d", date)) as month,
-  sum(totals.visits) as visits,
-  sum(totals.pageviews) as pageviews,
-  sum(totals.transactions) as transactions,
-from `bigquery-public-data.google_analytics_sample.ga_sessions_2017*`
-where _TABLE_SUFFIX between '0101' and '0331'
-group by 1
-order by 1;
